4bbba5250cfeaa9090534e3ca05e01d7857dc6e3
reifinal <reicocier@hotmail.com>
2016-11-23 00:08:12 -0600

    6) listado y detalles estudiantes.

--- pfinal/urls.py
+++ pfinal/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.lista_estudiantes),
+    url(r'^$', views.lista_estudiantes),
+    url(r'^estudiante/(?P<pk>[0-9]+)/$', views.detalle_estudiante),
     ]
--- pfinal/views.py
+++ pfinal/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Estudiante
 
 def lista_estudiantes(request):
     estudiantes = Estudiante.objects.all().order_by('apellido')
     return render(request, 'pfinal/lista_estudiantes.html', {'estudiantes':estudiantes})
+
+def detalle_estudiante(request, pk):
+        estudiantes = get_object_or_404(Estudiante, pk=pk)
+        return render(request, 'pfinal/detalle_estudiante.html', {'estudiantes': estudiantes})
 #
 #from django.contrib.auth.models import User
 #from django.contrib.auth import authenticate, login
