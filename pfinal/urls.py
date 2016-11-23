fb2a9cfa42309ed8f7f2432341c52947845db2c9
reifinal <reicocier@hotmail.com>
2016-11-23 17:26:06 -0600

    12)Sistema funcional CON disenio.

--- pfinal/urls.py
+++ pfinal/urls.py
@@ -3,12 +3,12 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', views.lista_estudiantes),
-    url(r'^estudiante/(?P<pk>[0-9]+)/$', views.detalle_estudiante),
+    url(r'^$', views.lista_estudiantes, name='lista_estudiante'),
+    url(r'^estudiante/(?P<pk>[0-9]+)/$', views.detalle_estudiante, name='detalle_estudiante'),
     url(r'^estudiante/nuevo/$', views.nuevo_estudiante, name='nuevo_estudiante'),
     url(r'^estudiante/(?P<pk>[0-9]+)/editar/$', views.editar_estudiante, name='editar_estudiante'),
-    url(r'^curso/$', views.lista_cursos),
-    url(r'^curso/(?P<pk>[0-9]+)/$', views.detalle_curso),
+    url(r'^curso/$', views.lista_cursos, name='lista_cursos'),
+    url(r'^curso/(?P<pk>[0-9]+)/$', views.detalle_curso, name='detalle_curso'),
     url(r'^curso/nuevo/$', views.nuevo_curso, name='nuevo_curso'),
     url(r'^curso/(?P<pk>[0-9]+)/editar/$', views.editar_curso, name='editar_curso'),
     url(r'^curso/(?P<pk>[0-9]+)/borrar/$', views.borrar_curso, name='borrar_curso'),
