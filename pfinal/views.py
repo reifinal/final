8163c5dd257f5f6f898908f1fe8c0c091a6af168
reifinal <reicocier@hotmail.com>
2016-11-22 21:52:21 -0600

    primera URL creada.

--- pfinal/views.py
+++ pfinal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+#from django.contrib.auth.models import User
+#from django.contrib.auth import authenticate, login
+## Create your views here.
+#
+#def authentication(request):
+#    if request.method == 'POST':
+#        action = request.POST.get('action',None)
+#        
